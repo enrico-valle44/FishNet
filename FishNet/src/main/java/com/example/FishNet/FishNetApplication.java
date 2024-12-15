4fe3a1c8cd2bc93da5d7fa8d6d3bec89c53a4b4c
enriu44 <e.2000valle@gmail.com>
2024-12-15 14:34:20 +0100

    First Commit

---
+++ FishNet/src/main/java/com/example/FishNet/FishNetApplication.java
@@ -0,0 +1,13 @@
+package com.example.FishNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishNetApplication.class, args);
+	}
+
+}
---
+++ FishNet/src/test/java/com/example/FishNet/FishNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FishNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
